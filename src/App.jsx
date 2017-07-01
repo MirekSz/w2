7192f47dfa37b227ad4d7546763ee35bdaf77cec
MirekSz <miro1994>
2017-07-01 16:55:41 +0200

    icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as emiter from 'event-emitter';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import { List, ListItem } from 'material-ui/List';
+import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
+import Badge from 'material-ui/Badge';
+import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
 let ee = emiter();
 setTimeout(() => {
     ee.emit('addItem', 5)
@@ -23,10 +31,11 @@ class AppContainer extends React.Component {
     }
 
     render() {
-        return (
+        return (<MuiThemeProvider>
             <div >
                 <ShoppingList {...this.state} />
             </div>
+        </MuiThemeProvider>
         );
     }
 }
@@ -39,16 +48,25 @@ function Square(props) {
 
 class ShoppingList extends React.Component {
     add() {
-        ee.emit('addItem', null)
+        ee.emit('addItem', this.refs._input.getValue())
     }
 
     render() {
         var items = this.props.items.map((i) => {
             return (<Square key={i} name={i} />);
         });
+        var items2 = this.props.items.map((i) => {
+            return (<ListItem key={i}><h3>{i}</h3></ListItem>);
+        });
         return (
             <div className="shopping-list">
-                <button onClick={this.add}>Add</button>
+                <TextField defaultValue="Item text" ref="_input" />
+                <RaisedButton label="Add" primary="true" onClick={this.add.bind(this)} />
+                <br />
+                <Badge badgeContent={items2.length} primary={true} >
+                    <NotificationsIcon />
+                </Badge>
+                <List>{items2}</List>
                 <h1>Shopping List for</h1>
                 <ul>
                     {items}
