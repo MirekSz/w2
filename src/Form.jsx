24584276122cd344793d6ef4c5ed20ef16208377
MirekSz <m.szajowski@gmail.com>
2017-07-03 16:47:36 +0200

    state

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,56 +2,65 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Form extends React.Component {
-      constructor(props) {
-            super(props);
-            this.state = { tempForm: {}, form: {} }
-      }
-      json() {
-            this.setState({ form: this.state.tempForm });
-      }
+    constructor(props) {
+        super(props);
+        this.state = {form: {}}
+        this.updateState = this.updateState.bind(this);
+    }
 
-      render() {
-            let j = JSON.stringify(this.state.form);
-            return (<div>
-                  {j}
-                  <Row>
-                        <Field label="First name" bind={this.state.tempForm} path="fn" />
-                        <Field label="Last name" bind={this.state.tempForm} path="ln" />
-                  </Row>
-                  <Field label="First name" bind={this.state.tempForm} path="fn" />
-                  <Field label="Last name" bind={this.state.tempForm} path="ln" />
-                  <button className="btn" onClick={this.json.bind(this)}>JSON</button>
-            </div>)
-      }
+
+    updateState(path, value) {
+        let form = this.state.form;
+        form[path] = value;
+        this.setState({form: form});
+    }
+
+    render() {
+        let j = JSON.stringify(this.state.form);
+        return (<div className="container">
+            {j}
+            <Row>
+                <Field label="First name" bind={this.state.form} path="fn" onValChange={this.updateState}/>
+                <Field label="Last name" bind={this.state.form} path="ln" onValChange={this.updateState}/>
+            </Row>
+            <Field label="First name" bind={this.state.form} path="fn" onValChange={this.updateState}/>
+            <Field label="Last name" bind={this.state.form} path="ln" onValChange={this.updateState}/>
+            <button className="btn btn-primary">Submit</button>
+        </div>)
+    }
 }
 class Row extends React.Component {
-      constructor(props) {
-            super(props);
-      }
-      render() {
-            let length = this.props.children.length;
-            return (<div>{React.Children.map(this.props.children, child => React.cloneElement(child, { elementsInRow: length }))})</div>);
-      }
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let length = this.props.children.length;
+        return (
+            <div>{React.Children.map(this.props.children, child => React.cloneElement(child, {elementsInRow: length}))}</div>);
+    }
 }
 class Field extends React.Component {
-      constructor(props) {
-            super(props);
-            this.handleChange = this.handleChange.bind(this);
-            this.state = { value: null }
-      }
-      handleChange(event) {
-            let v = event.target.value;
-            this.setState({ value: v });
-            this.props.bind[this.props.path] = v;
-      }
-      render() {
-            let classForColumns = 12 / this.props.elementsInRow;
-            return (<div className={'form-group col-xs-' + classForColumns} W>
-                  <label >{this.props.label}</label>
-                  <input type="text" className="form-control" value={this.state.value} onChange={this.handleChange} />
-            </div>);
-      }
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        let v = event.target.value;
+        this.props.onValChange(this.props.path, v)
+    }
+
+    render() {
+        let classForColumns = 12 / this.props.elementsInRow;
+        return (<div className={'form-group col-xs-' + classForColumns}>
+            <label >{this.props.label}</label>
+            <input type="text" className="form-control" value={this.props.bind[this.props.path]}
+                   onChange={this.handleChange}/>
+        </div>);
+    }
 }
 Field.propTypes = {
-      label: PropTypes.string.isRequired
-}
+    label: PropTypes.string.isRequired,
+    onValChange: PropTypes.func.isRequired
+};
