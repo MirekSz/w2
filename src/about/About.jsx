2332d4cfd24906799b7119308e70c2c95eb230ce
MirekSz <miro1994>
2017-07-21 20:29:01 +0200

    id

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -19,29 +19,29 @@ const dataSource = ['Urlop', 'Święto', 'Opieka', 'Chorobowe'];
 
 let days = [];
 for (let i = 0; i <= 31; i++) {
-    let day = {day: i, offReason: ''};
+    let day = { day: i, offReason: '' };
     days.push(day);
 }
 
 export default class About extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {days, project: 'Next Verto', time: 0};
+        this.state = { days, project: 'Next Verto', time: 0 };
         this.add$ = new Rx.Subject();
         this.add$.debounceTime(500).distinctUntilChanged().subscribe((val) => {
-            this.setState({project: val.project});
+            this.setState({ project: val.project });
         });
     }
 
     componentWillUpdate() {
         this.time = Date.now();
-//        Perf.start();
+        //        Perf.start();
     }
 
     componentDidUpdate(prevProps, prevState) {
-//        Perf.stop();
-//        Perf.printInclusive();
-//        Perf.printWasted();
+        //        Perf.stop();
+        //        Perf.printInclusive();
+        //        Perf.printWasted();
 
         this.refreshTimes(this.state.time === prevState.time);
     }
@@ -63,18 +63,18 @@ export default class About extends React.Component {
         if (index > -1) {
             days[index] = Object.assign({}, day);
         }
-        this.setState({days});
+        this.setState({ days });
     };
 
     handleProjectInput(event) {
-        this.add$.next({project: event.target.value, stateHolder: this});
+        this.add$.next({ project: event.target.value, stateHolder: this });
     };
 
     render() {
         let rows = [];
         for (let day of days) {
-            rows.push(<CalendarItem day={day} project={this.state.project}
-                                    onOffReasonChange={this.onOffReasonChange.bind(this)}/>
+            rows.push(<CalendarItem key={day.day} day={day} project={this.state.project}
+                onOffReasonChange={this.onOffReasonChange.bind(this)} />
                 /*<TableRow>
                     <TableRowColumn>{day.day}</TableRowColumn>
                     <TableRowColumn>{this.state.project}</TableRowColumn>
@@ -95,20 +95,19 @@ export default class About extends React.Component {
             <MuiThemeProvider>
                 <div>
                     <TextField floatingLabelText="Projekt" defaultValue={this.state.project}
-                               onChange={this.handleProjectInput.bind(this)}/>
+                        onChange={this.handleProjectInput.bind(this)} />
                     <h3 className="times pull-right">Last Update Time: {this.state.time}</h3>
-                    <Table adjustForCheckbox={false}>
+                    <Table>
                         <TableHeader displaySelectAll={false}>
                             <TableRow>
-                                <TableHeaderColumn style="align:center">Dzien</TableHeaderColumn>
+                                <TableHeaderColumn>Dzien</TableHeaderColumn>
                                 <TableHeaderColumn>Projekt</TableHeaderColumn>
                                 <TableHeaderColumn>Czas pracy</TableHeaderColumn>
                                 <TableHeaderColumn>Czas nieobecnosci</TableHeaderColumn>
                                 <TableHeaderColumn>Przyczyna nieobecnosci</TableHeaderColumn>
                             </TableRow>
                         </TableHeader>
-                        <TableBody displayRowCheckbox={false}
-                                   adjustForCheckbox={false}>
+                        <TableBody displayRowCheckbox={false}>
                             {rows}
                         </TableBody>
                     </Table>
